eb91f5cb84ea4bc986d20f4e731dd250d11275d9
ShuYong Duan <duanshuyong0@163.com>
2015-02-04 19:38:17 +0800

    add for DATEGRID STUDY
    add for DATEGRID STUDY

--- DataGrid/jQuery.hello.DataGrid.js
+++ DataGrid/jQuery.hello.DataGrid.js
@@ -6,4 +6,48 @@
 
 
 
-jQuery.fn.
+jQuery.fn.extend({
+           DataGridClear:function(){
+           $(this).RepeaterClear();
+           },
+
+           DataGridSetItemClass:function(class1,class2,hoverClass){
+           $(this).RepeaterSetItemClass(class1,class2,hoverClass);
+           },
+      
+
+          //
+          DataGridSetWidth:function(width){
+          $(this).data('MyTable').width(width);
+          $(this).data('TableBodyArea').width(width);
+          $(this).data('TableHeadArea').width(width);
+          },
+             
+          //
+          DateGrid:function(width,height,dt){
+             this.each(function(){
+              
+              var TableBody=$(this);
+              var MyTableId="hello-table-"+TableBody.attr('id');
+              if($(this).data('Drawd')){
+              	TableBody.Repeater(dt);
+              	TableBody.find('.itemtemplate').slice(1).find('td').css('width', 'auto');
+              	var tds=TableBody.data('TableHead').find('td');
+              	tds.each(function(index, el) {
+              		var td =$(this);
+              	//	$('string/element/array/function/jQuery object/string, context')
+              	     $(TableBody.find('tr:first').find('td')[tds.index(td)]).css('width',td.css('width'));
+              	});
+              	return;
+              }
+
+              // 开始渲染表格=========================================
+              //
+              TableBody.addClass('TableBody');
+              
+
+
+             });
+
+          }   
+});
---
+++ DataGrid/jQuery.hello.Repeater.js
@@ -0,0 +1,53 @@
+/*
+  creator:duansy
+*/
+
+jQuery.fn.extend({
+         
+
+        
+
+
+         // RepeaterCleat:
+        RepeaterCleat:function(){
+        this.each(function(index, val) {
+        	 /* iterate through array or object */
+        	 if($(this).data("_ItemTemplate")==null){
+                $(this).data("_ItemTemplate",$(this).find(".itemtemplate").toHTML());
+        	 }
+        	 $(this).find('.itemtemplate').remove();
+        });
+        },
+
+
+        //RepeaterSetItemClass:
+        RepeaterSetItemClass:function(class1,class2,hoverClass){
+            this.each(function() {
+            	 /* iterate through array or object */
+                 if(class1==null || class2==null || hoverClass==null)
+                 	return;
+                    
+                    $(this).data('_class1', class1);
+                    $(this).data('_class2', class2);
+                    $(this).data('_hoverClass', hoverClass);
+                 //
+                    if($(this).data("_DataSrc")!=null){
+                    	var domtype=$(this).find(".itemtemplate").attr('nodeName');
+
+                    	$(this).find("domtype").addClass("class1");
+                    	$(this).find(domtype+":nth-child(evev)").addClass("class2");
+
+                    	$(this).find(domtype).hover(
+                    		function(){
+                            $(this).addClass(hoverClass);
+                    		},
+                    		function(){
+                             $(this).removeClass(hoverClass);
+                    		}
+                    		);
+                    }
+            });
+        },
+
+             
+});
